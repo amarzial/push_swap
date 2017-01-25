934c2b2601c0ac324c00e45a1c8ccb85ca5c3b6f
Alessandro MARZIALETTI <amarzial@e3r11p19.42.fr>
2017-01-25 16:36:45 +0100

    not n2

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/13 18:53:43 by amarzial          #+#    #+#             */
-/*   Updated: 2017/01/24 14:39:36 by amarzial         ###   ########.fr       */
+/*   Updated: 2017/01/25 16:28:23 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,20 @@
 # include "bonus.h"
 # include "libft.h"
 
+typedef struct	s_algo
+{
+	size_t	cnt;
+	int		avg;
+	size_t	splits[2];
+	int		rot;
+}				t_algo;
+
 int		exec_cmd(char *cmd, t_stack *a, t_stack *b);
 int		validarg(const char *arg, t_stack *stack);
 void	error_exit(t_stack *stack);
 int		perform(char *op, t_stack *a, t_stack *b, t_opts *opt);
 void	sort_stack(t_stack *a, t_stack *b, t_opts *opt);
+void	upper(t_stack *a, t_stack *b, size_t size, t_opts *opt);
+void	lower(t_stack *a, t_stack *b, size_t size, t_opts *opt);
 
 #endif
--- srcs/algorithm.c
+++ srcs/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 14:22:11 by amarzial          #+#    #+#             */
-/*   Updated: 2017/01/24 20:05:56 by amarzial         ###   ########.fr       */
+/*   Updated: 2017/01/25 16:36:09 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,82 @@
 #include "push_swap.h"
 #include "bonus.h"
 #include "mystack.h"
-/*
-static int		place(t_stack *a, int top, int *lims)
-{
-	int		res;
 
-	if ((top > lims[0] && top < lims[1]) && (*(int*)a->begin->content > top && \
-		*(int*)a->end->content < top))
-		res = 1;
-	else if (top > lims[1] && (*(int*)a->begin->content == lims[0]))
-		res = 1;
-	else if (top < lims[0] && (*(int*)a->end->content == lims[1]))
-		res = 1;
-	else
-		res = 0;
-	if (res)
+static int		stack_avg(t_stack *stack, size_t size)
+{
+	int		sum;
+	size_t	cnt;
+	t_list	*lst;
+
+	sum = 0;
+	lst = stack->begin;
+	cnt = size;
+	while (cnt)
 	{
-		lims[0] = ft_min(top, lims[0]);
-		lims[1] = ft_max(top, lims[1]);
+		sum += *(int*)lst->content;
+		lst = lst->next;
+		cnt--;
 	}
-	return (res);
+	return (sum / size);
 }
-*/
+
+void			upper(t_stack *a, t_stack *b, size_t size, t_opts *opt)
+{
+	t_algo vars;
+
+	if (size <= 0)
+		return ;
+	if (size == 1 && perform("pa", a, b, opt))
+		return ;
+	ft_bzero(&vars, sizeof(t_algo));
+	vars.avg = stack_avg(b, size);
+	while (vars.cnt++ < size)
+	{
+		if ((*(int*)b->begin->content > vars.avg) && ((vars.splits[0]++) || 1))
+			perform("pa", a, b, opt);
+		else if ((vars.splits[1]++) || 1)
+		{
+			perform("rb", a, b, opt);
+			vars.rot++;
+		}
+	}
+	vars.rot %= stack_size(b);
+	while (vars.rot--)
+		perform("rrb", a, b, opt);
+	lower(a, b, vars.splits[0], opt);
+	upper(a, b, vars.splits[1], opt);
+}
+
+void			lower(t_stack *a, t_stack *b, size_t size, t_opts *opt)
+{
+	t_algo	vars;
+	if (size <= 1)
+		return ;
+	ft_bzero(&vars, sizeof(t_algo));
+	vars.avg = stack_avg(a, size);
+	while (vars.cnt++ < size)
+	{
+		if ((*(int*)a->begin->content <= vars.avg) && ((vars.splits[1]++) || 1))
+			perform("pb", a, b, opt);
+		else if ((vars.splits[0]++) || 1)
+		{
+			perform("ra", a, b, opt);
+			vars.rot++;
+		}
+	}
+	vars.rot %= stack_size(a);
+	while (vars.rot--)
+		perform("rra", a, b, opt);
+	lower(a, b, vars.splits[0], opt);
+	upper(a, b, vars.splits[1], opt);
+}
+
 void			sort_stack(t_stack *a, t_stack *b, t_opts *opt)
 {
+	size_t	size;
 
-	(void)a;
-	(void)b;
-	(void)opt;
+	size = stack_size(a);
+	if (size == 1)
+		return ;
+	lower(a, b, size, opt);
 }
